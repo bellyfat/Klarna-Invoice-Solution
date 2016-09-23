8ae95a0b1a91f39f0bedda9ff68c0626581becd3
Mattias Nording <mattiasnording@gmail.com>
2016-09-23 22:55:36 +0200

    Sorting out install DB and some styling

--- install/index.php
+++ install/index.php
@@ -18,33 +18,62 @@ if(isset($_POST["install"])) {
     if (!$dblink) {
         die("Incorrect Params - Could not connect");
     }
-    file_put_contents("../db.php", "<?php \$dblink = mysqli_connect(\"" . $server . "\", \"" . $user . "\", \"" . $pass . "\", \"" . $prefix . "kpm\"); ?>");
-   $res = mysqli_query($dblink,"CREATE DATABASE IF NOT EXISTS `".$prefix."kpm` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;");
+    file_put_contents("../db.php", "<?php \$dblink = mysqli_connect(\"" . $server . "\", \"" . $user . "\", \"" . $pass . "\", \"" . $prefix . "_kpm\"); ?>");
+   $res = mysqli_query($dblink,"CREATE DATABASE IF NOT EXISTS `".$prefix."_kpm` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;");
     echo mysqli_error($dblink);
-    $dblink = mysqli_connect($server, $user, $pass,$prefix."kpm");
-    $sql = "CREATE TABLE IF NOT EXISTS `store` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
+    $dblink = mysqli_connect($server, $user, $pass,$prefix."_kpm");
+    $sql = 'CREATE TABLE `store` (
+          `id` int(11) NOT NULL,
           `name` varchar(255) NOT NULL,
           `eid` int(11) NOT NULL,
           `shared` varchar(255) NOT NULL,
-          `testmode` int(11) NOT NULL,
-          PRIMARY KEY (`id`)
-        ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;";
+          `testmode` int(11) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
     mysqli_query($dblink,$sql);
     echo mysqli_error($dblink);
-    $sql = "CREATE TABLE IF NOT EXISTS `user` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
+    $sql = 'CREATE TABLE `user` (
+          `id` int(11) NOT NULL,
           `email` varchar(255) NOT NULL,
           `passwordhash` varchar(255) NOT NULL,
-          `level` int(11) NOT NULL,
-          PRIMARY KEY (`id`)
-        ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;";
+          `level` int(11) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
     mysqli_query($dblink,$sql);
     echo mysqli_error($dblink);
-    $sql="CREATE TABLE IF NOT EXISTS `user_stores` (
+    $sql='CREATE TABLE `user_stores` (
           `userid` int(11) NOT NULL,
           `storeid` int(11) NOT NULL
-        ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
+    mysqli_query($dblink,$sql);
+    echo mysqli_error($dblink);
+    $sql = 'ALTER TABLE `store`
+  ADD PRIMARY KEY (`id`);';
+    mysqli_query($dblink,$sql);
+    echo mysqli_error($dblink);
+    $sql='
+   ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);';
+    mysqli_query($dblink,$sql);
+    echo mysqli_error($dblink);
+    $sql='
+  ALTER TABLE `user_stores`
+  ADD KEY `userid` (`userid`),
+  ADD KEY `storeid` (`storeid`);';
+    mysqli_query($dblink,$sql);
+    echo mysqli_error($dblink);
+    $sql='
+ALTER TABLE `store`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT';
+    mysqli_query($dblink,$sql);
+    echo mysqli_error($dblink);
+    $sql='
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT';
+    mysqli_query($dblink,$sql);
+    echo mysqli_error($dblink);
+    $sql='
+  ALTER TABLE `user_stores`
+  ADD CONSTRAINT `store_exists` FOREIGN KEY (`storeid`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_exists` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;';
     mysqli_query($dblink,$sql);
     echo mysqli_error($dblink);
     $createHash = password_hash($adminpass."".sha1($adminpass),PASSWORD_BCRYPT);
@@ -54,13 +83,30 @@ if(isset($_POST["install"])) {
     echo mysqli_error($dblink);
 }
 ?>
+<link rel="stylesheet" href="../styles/foundation.min.css">
+<link rel="stylesheet" href="../styles/main.css">
+<div class="row">
 
-<form method="post">
+    <form method="post">
+    <div class="large-3 small-12 columns">
     <input type="text" placeholder="server" name="server">
+    </div>
+    <div class="large-3 small-12 columns">
     <input type="text" placeholder="prefix" name="prefix">
+    </div>
+    <div class="large-3 small-12 columns">
     <input type="text" placeholder="user" name="user">
+    </div>
+    <div class="large-3 small-12 columns">
     <input type="text" placeholder="pass" name="pass">
+    </div>
+    <div class="large-3 small-12 columns">
     <input type="text" placeholder="adminuser" name="adminuser">
+    </div>
+    <div class="large-3 small-12 columns">
     <input type="text" placeholder="adminpass" name="adminpass">
-    <input type="submit" name="install" value="Install"/>
+    </div>
+    <input type="submit" class="button" name="install" value="Install"/>
 </form>
+
+</div>
