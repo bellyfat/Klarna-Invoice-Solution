6862556ecd80ddeff5e27a6d8c0772a61acf0637
Mattias Nording <mattiasnording@gmail.com>
2016-10-04 16:29:22 +0200

    Adding logo

--- menu.php
+++ menu.php
@@ -8,6 +8,7 @@
 ?>
 <div class="menu">
     <div class="row">
+        <span class="icon"><img src="https://cdn.klarna.com/1.0/shared/image/generic/logo/sv_se/basic/white.png?width=130"></span>
         <ul class="">
             <li><a href="index.php">Place order</a></li>
             <li><a href="ordersearch.php">Search Order</a></li>
